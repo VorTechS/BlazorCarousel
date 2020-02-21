473c841ff4a6f510e629245f77112700d117a96a
Stuart Massey (VorTechS) <vortechs@blueyonder.co.uk>
2020-02-21 10:01:39 +0000

    Initial Commit - The basic carousel seems to work, so before breaking it, this seems a good starting point!

---
+++ Component/ImageFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Components
+{
+    public class ImageFile
+    {
+        public string MimeType { get; set; }
+
+        public byte[] FileContent { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Url { get; set; }
+
+        public string Base64Image
+        {
+            get
+            {
+                string convertedContent = null;
+
+                if (this.FileContent != null)
+                {
+                    convertedContent = $"data:{this.MimeType};base64,{Convert.ToBase64String(this.FileContent)}";
+                }
+
+                return convertedContent;
+            }
+        }
+
+    }
+}
